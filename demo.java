b86d6167bae999410ee87e969456be3caf1e97ef
shubham vishwakarma <shubham10137@gmail.com>
2026-03-25 23:33:58 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,26 @@
+public class demo {
+    public static void main(String[] args) {
+        //while loop
+         int i = 11;
+        while(i<=10){
+            System.out.println(i);
+            i--;
+        }
+
+
+        // do while loop
+        
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while(i<=10);
+
+
+        //for loop
+        for(int j=0; j<=20; j++){
+            System.out.println(j);
+        }
+    }
+    
+}
---
+++ demo2.java
@@ -0,0 +1,53 @@
+public class demo2 {
+    public static void main(String[] args) {
+        // Nested loop to print patterns
+    for(int i=1; i<=10; i++){
+        for(int j=1; j<=i; j++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+
+    //jump statement in java
+    // break, continue
+    for(int i=1; i<=10; i++){
+        System.out.println(i);
+        if(i==5){
+            break;
+
+        } 
+    }
+
+    for(int i=1; i<=10; i++){
+        if(i==5){
+            continue;
+        }
+        System.out.println(i);
+    }
+
+    // break in  nested loops
+    for(int i=1; i<=10; i++){
+        for(int j=1; j<=i; j++){
+            System.out.print("* ");
+            if(j>=5){
+                break;
+            }
+        }
+        System.out.println();
+    }
+
+    //lables
+    outer: for(int i=1; i<=10; i++){
+       inner: for(int j=1; j<=i; j++){
+            System.out.print("* ");
+            if(j>=5){
+                break outer;
+            }
+        }
+        System.out.println();
+    }
+
+
+
+}
+}
